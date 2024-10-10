8b37e6cee50c61fbc2a2e26b93f46e75a6318d75
Surya-2811 <84892906+surya-2811@users.noreply.github.com>
2024-10-10 23:18:49 +0530

    Update chunk.js

--- Easy/chunk/chunk.js
+++ Easy/chunk/chunk.js
@@ -4,27 +4,23 @@
  * @param {number} [size=1] The length of each chunk.
  * @returns {Array<Array<T>>} The new array of chunks.
  */
-function chunk(array, size = 1) {
-    if (!Array.isArray(array) || size < 1) {
-      return [];
-    }
-  
-    const result = [];
-    let chunk = [];
-  
-    for (let i = 0; i < array.length; i++) {
-      chunk.push(array[i]);
-      if (chunk.length === size || i === array.length - 1) {
-        result.push(chunk);
-        chunk = [];
-      }
-    }
-  
-    return result;
+function chunk(arr, size =1){
+  if(!Array.isArray(arr) || size <1){
+    return []
+  }
+  const result =[]
+  let chunk = [];
+  for(let i =0; i< arr.length; i++){
+    chunk.push(arr[i])
+     if(chunk.length === size || i === arr.length - 1){
+      result.push(chunk);
+      chunk = []
+     }  }
+  return result
 }
 
-chunk(['a', 'b', 'c', 'd']); // => [['a'], ['b'], ['c'], ['d']]
-chunk([1, 2, 3, 4], 2); // => [[1, 2], [3, 4]]
-chunk([1, 2, 3, 4], 3); // => [[1, 2, 3], [4]]
+console.log(chunk(['a', 'b', 'c', 'd'])); // => [['a'], ['b'], ['c'], ['d']]
+console.log(chunk([1, 2, 3, 4], 2)); // => [[1, 2], [3, 4]]
+console.log(chunk([1, 2, 3, 4], 3)); // => [[1, 2, 3], [4]]
 
   
