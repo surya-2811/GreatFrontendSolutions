c7ca938998ac7d6a40cdde663dd850ee8b439309
Surya-2811 <84892906+surya-2811@users.noreply.github.com>
2024-10-10 23:59:28 +0530

    Update clamp.js

--- Easy/clamp/clamp.js
+++ Easy/clamp/clamp.js
@@ -7,3 +7,12 @@
 function clamp(value, lower, upper) {
     return Math.min(upper, Math.max(lower, value))
 }
+
+// Within the bounds, return as-is.
+console.log(clamp(3, 0, 5)); // => 3
+
+// Smaller than the lower bound.
+console.log(clamp(-10, -3, 5)); // => -3
+
+// Bigger than the upper bound.
+console.log(clamp(10, -5, 5)); // => 5
