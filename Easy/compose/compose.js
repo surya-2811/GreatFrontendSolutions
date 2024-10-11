3f92e2ce983db89816c96c1a8ffad4475f691127
Surya-2811 <84892906+surya-2811@users.noreply.github.com>
2024-10-11 23:41:17 +0530

    Update compose.js

--- Easy/compose/compose.js
+++ Easy/compose/compose.js
@@ -4,30 +4,17 @@
  * @returns Function
  */
 
-function compose(...fns) {
-    return function (x) {
-      let result = x;
-  
-      for (let i = fns.length - 1; i >= 0; i--) {
-        result = fns[i](result);
-      }
-  
-      return result;
-    };
-  }
+function compose(...args) {
+  return function (x) {
+    let result = x;
 
-  
-/**
- * Solution - 2
- * @param {...Function} args
- * @returns Function
- */
+    for (let i = args.length - 1; i >= 0; i--) {
+      result = args[i](result);
+    }
+    return result;
+  };
+}
 
-function compose(...fns) {
-    return function (x) {
-      return fns.reduceRight((result, func) => func(result), x);
-    };
-  }
   
 
 const add1 = (num) => num + 1;
@@ -35,5 +22,5 @@ const double = (num) => num * 2;
 const subtract10 = (num) => num - 10;
 
 const composedFn = compose(subtract10, double, add1);
-composedFn(3); // (3 + 1) * 2 - 10 => -2
+console.log(composedFn(3)); // (3 + 1) * 2 - 10 => -2
 
