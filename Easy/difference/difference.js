33409d5579082668732a69297c8131009f9befc4
Surya-2811 <84892906+surya-2811@users.noreply.github.com>
2024-10-11 23:52:32 +0530

    Update difference.js

--- Easy/difference/difference.js
+++ Easy/difference/difference.js
@@ -4,23 +4,10 @@
  * @return {Array} Returns filtered array.
  */
 function difference(array, values) {
-    const result = [];
-  
-    // Create a set of all the values in the values arrays.
-    const valuesSet = new Set(values);
-  
-    for (let i = 0; i < array.length; i++) {
-      const value = array[i];
-      // Check if the value is in the values set.
-      if (!valuesSet.has(value) && !(value === undefined && !(i in array))) {
-        result.push(value);
-      }
-    }
-  
-    return result;
+  return array.filter(item => !values.includes(item))
 }
  
-difference([1, 2, 3], [2, 3]); // => [1]
-difference([1, 2, 3, 4], [2, 3, 1]); // => [4]
-difference([1, 2, 3], [2, 3, 1, 4]); // => []
-difference([1, , 3], [1]); // => [3]
+console.log(difference([1, 2, 3], [2, 3])); // => [1]
+console.log(difference([1, 2, 3, 4], [2, 3, 1])); // => [4]
+console.log(difference([1, 2, 3], [2, 3, 1, 4])); // => []
+console.log(difference([1, , 3], [1])); // => [3]
