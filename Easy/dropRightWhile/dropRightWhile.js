ea296d9a7ee3c84e336abe7f24a5d10c5f1b0bfb
Surya-2811 <84892906+surya-2811@users.noreply.github.com>
2024-10-12 21:43:03 +0530

    Update dropRightWhile.js

--- Easy/dropRightWhile/dropRightWhile.js
+++ Easy/dropRightWhile/dropRightWhile.js
@@ -4,17 +4,15 @@
  * @return {Array} Returns the slice of `array`.
  */
 function dropRightWhile(array, predicate) {
-    let index = array.length - 1;
-  
-    while (index >= 0 && predicate(array[index], index, array)) {
-      index--;
-    }
-  
-    return array.slice(0, index + 1);
+  let index = array.length -1;
+  while(index >=0 && predicate(array[index])){
+    index--
+  }
+  return array.slice(0, index+1)
 }
 
-dropRightWhile([1, 2, 3, 4, 5], (value) => value > 3); // => [1, 2, 3]
-dropRightWhile([1, 2, 3], (value) => value < 6); // => []
-dropRightWhile([1, 2, 3, 4, 5], (value) => value > 6); // => [1, 2, 3, 4, 5]
+console.log(dropRightWhile([1, 2, 3, 4, 5], (value) => value > 3)); // => [1, 2, 3]
+console.log(dropRightWhile([1, 2, 3], (value) => value < 6)); // => []
+console.log(dropRightWhile([1, 2, 3, 4, 5], (value) => value > 6)); // => [1, 2, 3, 4, 5]
 
   
