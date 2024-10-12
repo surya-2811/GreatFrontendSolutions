28a08377844985413928f49f28903dfffbebe478
Surya-2811 <84892906+surya-2811@users.noreply.github.com>
2024-10-12 23:10:59 +0530

    Update fromPairs.js

--- Easy/fromPairs/fromPairs.js
+++ Easy/fromPairs/fromPairs.js
@@ -9,33 +9,18 @@
  * @returns {Object} - The object composed from the key-value pairs.
  */
 function fromPairs(pairs) {
-    const result = {};
-  
-    for (const [key, value] of pairs) {
-      result[key] = value;
-    }
+  let result ={};
+  for(let [key, value] of pairs){
+    result[key] = value
+  }
   
     return result;
 }
 
-// Solution - 2
-
-/**
- * Creates an object from an array of key-value pairs.
- *
- * @param {Array} pairs - An array of key-value pairs.
- * @returns {Object} - The object composed from the key-value pairs.
- */
-
-function fromPairs(pairs) {
-    return Object.fromEntries(pairs);
-}
-  
 const pairs = [
-    ['a', 1],
-    ['b', 2],
-    ['c', 3],
-  ];
-  
-fromPairs(pairs); // => { a: 1, b: 2, c: 3 }
+  ['a', 1],
+  ['b', 2],
+  ['c', 3],
+];
+console.log(fromPairs(pairs)); // => { a: 1, b: 2, c: 3 }
   
