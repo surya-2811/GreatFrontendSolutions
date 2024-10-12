9c94e5b0a0565734fde231a153cca7133550f2da
Surya-2811 <84892906+surya-2811@users.noreply.github.com>
2024-10-12 22:11:18 +0530

    Update fill.js

--- Easy/fill/fill.js
+++ Easy/fill/fill.js
@@ -8,27 +8,22 @@
  */
 
 function fill(array, value, start = 0, end = array.length) {
-    const length = array.length;
-  
-    if (start < 0) {
-      start = -start > length ? 0 : length + start;
-    }
-  
-    if (end > length) {
-      end = length + 1;
-    }
-  
-    if (end < 0) {
-      end += length;
-    }
-  
-    for (let i = start; i < Math.min(end, array.length); i++) {
-      array[i] = value;
-    }
-  
-    return array;
+  let length = array.length;
+  if(start < 0){
+    start = -start >length ? 0 : start + length
+  }
+  if(end > length){
+    end = length +1
+  }
+  if(end < 0){
+    end = end + length
+  }
+   for(let i = start; i< end; i++){
+    array[i] = value
+   }
+  return array
 }
   
-fill([1, 2, 3], 'a'); // ['a', 'a', 'a']
-fill([4, 6, 8, 10], '*', 1, 3); // [4, '*', '*', 10]
-fill([4, 6, 8, 10, 12], '*', -3, -1); // [4, 6, '*', '*', 12]
+console.log(fill([1, 2, 3], 'a')); // ['a', 'a', 'a']
+console.log(fill([4, 6, 8, 10], '*', 1, 3)); // [4, '*', '*', 10]
+console.log(fill([4, 6, 8, 10, 12], '*', -3, -1)); // [4, 6, '*', '*', 12]
