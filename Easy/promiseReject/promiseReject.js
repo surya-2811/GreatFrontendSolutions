19764813c7ce65042dcfeb032b4f8a1450fa4c72
Surya-2811 <84892906+surya-2811@users.noreply.github.com>
2024-10-13 21:08:36 +0530

    Update promiseReject.js

--- Easy/promiseReject/promiseReject.js
+++ Easy/promiseReject/promiseReject.js
@@ -1,5 +1,13 @@
+function promiseReject(arg){
+    return new Promise((_, reject)=>{
+          reject(arg)
+    })
+}
+
 try {
-    promiseReject('Mayday!');
+    promiseReject('Mayday!').catch((err)=>{
+        console.log(err)
+    });
 } catch (err) {
     console.log(err); // Mayday!
 }
