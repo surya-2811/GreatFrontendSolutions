c746a73919df602842f1d3c56bd83967577cfae1
Surya-2811 <84892906+surya-2811@users.noreply.github.com>
2024-10-13 12:56:09 +0530

    Update inRange.js

--- Easy/inRange/inRange.js
+++ Easy/inRange/inRange.js
@@ -6,25 +6,21 @@
  */
 
 function inRange(value, startParam, endParam) {
-    let start = startParam;
-    let end = endParam;
-    if (end === undefined) {
-      end = start;
-      start = 0;
-    }
-  
-    if (start < end) {
-      return value >= start && value < end;
-    }
-  
-    return value >= end && value < start;
+  let start = startParam;
+  let end = endParam;
+  if(endParam === undefined){
+    start =0;
+    end = startParam
+  }
+  if(value >= start && value <end) return true;
+  return value >=end && value < start
 }
   
 
-inRange(3, 2, 4); // => true
-inRange(4, 8); // => true
-inRange(4, 2); // => false
-inRange(2, 2); // => false
-inRange(1.2, 2); // => true
-inRange(5.2, 4); // => false
-inRange(-3, -2, -6); // => true
+console.log(inRange(3, 2, 4)); // => true
+console.log(inRange(4, 8)); // => true
+console.log(inRange(4, 2)); // => false
+console.log(inRange(2, 2)); // => false
+console.log(inRange(1.2, 2)); // => true
+console.log(inRange(5.2, 4)); // => false
+console.log(inRange(-3, -2, -6)); // => true
