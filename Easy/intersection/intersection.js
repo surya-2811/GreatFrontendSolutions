de48ad59cd34c2402d48aac74ed437c4299044de
Surya-2811 <84892906+surya-2811@users.noreply.github.com>
2024-10-13 13:32:21 +0530

    Update intersection.js

--- Easy/intersection/intersection.js
+++ Easy/intersection/intersection.js
@@ -5,21 +5,14 @@
  */
 
 function intersection(...arrays) {
-    if (arrays.length === 0) {
-      return [];
-    }
-  
-    const set = new Set(arrays[0]);
-  
-    for (let i = 1; i < arrays.length; i++) {
-      set.forEach((value) => {
-        if (!arrays[i].includes(value)) {
-          set.delete(value);
-        }
-      });
-    }
-  
-    return Array.from(set);
+  if (!arrays.length) return [];
+  let [array, ...restOfArray] = arrays;
+  let result = array.filter((item) => {
+    return restOfArray.every((array) => {
+      return array.includes(item);
+    });
+  });
+  return result;
 }
   
 
