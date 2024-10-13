73a773a78fc8185d5ef1bf0693ebe0134e2d8033
Surya-2811 <84892906+surya-2811@users.noreply.github.com>
2024-10-13 21:13:20 +0530

    Update uniqueArray.js

--- Easy/uniqueArray/uniqueArray.js
+++ Easy/uniqueArray/uniqueArray.js
@@ -1,15 +1,14 @@
 function uniqueArray(array) {
-    const result = [];
-  
-    array.forEach((item) => {
-      if (!result.includes(item)) {
-        result.push(item);
-      }
-    });
-  
-    return result;
+  const result = [];
+  array.forEach((item) => {
+    if (!result.includes(item)) {
+      result.push(item);
+    }
+  });
+
+  return result;
 }
 
-uniqueArray([1, 2, 3]); // [1, 2, 3]
-uniqueArray([1, 1, 2]); // [1, 2]
-uniqueArray([2, 1, 2]); // [2, 1]
+console.log(uniqueArray([1, 2, 3])); // [1, 2, 3]
+console.log(uniqueArray([1, 1, 2])); // [1, 2]
+console.log(uniqueArray([2, 1, 2])); // [2, 1]
